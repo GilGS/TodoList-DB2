7892cce09ff8608b70505595941f4d094dc3dfc8
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-07-22 08:43:41 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-        .Package(url: "https://github.com/IBM-DTeam/swift-for-db2.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/todolist-api", majorVersion: 0)
+        .Package(url: "https://github.com/IBM-DTeam/swift-for-db2.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/todolist-api", majorVersion: 0, minor: 3)
     ]
 )
